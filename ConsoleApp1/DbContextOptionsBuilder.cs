cc4345ba901625b14e1dda2de0587d27b8fbd8f3
Ahmed Hafez <Ahmed.abdelghany.Hafez@gmail.com>
2022-01-09 19:49:12 +0200

    Add project files.

---
+++ ConsoleApp1/DbContextOptionsBuilder.cs
@@ -0,0 +1,9 @@
+﻿using EF_Activity001;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp1
+{
+    internal class DbContextOptionsBuilder<T1, T2> : DbContextOptionsBuilder<AdventureWorksContext>
+    {
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using EF_Activity001;
+using InventoryHelpers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static IConfigurationRoot _configuration;
+        static DbContextOptionsBuilder<AdventureWorksContext>
+        _optionsBuilder;
+        static void BuildOptions()
+        {
+            _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
+            _optionsBuilder = new DbContextOptionsBuilder<AdventureWorksContext>();
+            _optionsBuilder.UseSqlServer(_configuration.GetConnectionString
+            ("AdventureWorks"));
+        }
+        static void Main(string[] args)
+        {
+            BuildOptions();
+            Console.WriteLine("Would you like to view all salespeople? [y / n]");
+
+            var input = Console.ReadLine();
+            if (input.StartsWith("y", StringComparison.
+            OrdinalIgnoreCase))
+            {
+                ShowAllSalesPeople();
+            }
+        }
+        private static void ShowAllSalesPeople()
+        {
+            using (var db = new AdventureWorksContext(_optionsBuilder.
+            Options))
+            {
+                var salesPeople = db.SalesPeople
+                    .Include(x => x.BusinessEntity)
+                    .ThenInclude(y => y.BusinessEntity)
+                    .Take(20).ToList();
+
+                foreach (var sp in salesPeople)
+                {
+
+                    Console.WriteLine($"{sp} | {sp.BusinessEntity.BusinessEntity.LastName}" + $", {sp.BusinessEntity.BusinessEntity.FirstName}");
+
+                }
+            }
+        }
+    }
+}
